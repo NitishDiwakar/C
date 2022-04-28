1dc187acf440d98eff1c288811f14227e1897bfe
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2022-04-28 12:12:05 +0530

    Bubble sort of user input numbers

---
+++ bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[50], num, x, y, temp;
+
+	printf("Please Enter the Number of Elements you want in the array: ");
+	scanf("%d", &num);
+
+	printf("Please Enter the Value of Elements: \n");
+
+	for(x = 0; x  < num; x++)
+	{
+		scanf("%d", &arr[x]);
+	}
+
+	for(x = 0; x < num - 1; x++)
+	{
+		for(y = 0; y < num-x-1; y++)
+		{
+			if(arr[y]>arr[y+1])
+			{
+				temp = arr[y];
+				arr[y] = arr[y+1];
+				arr[y+1] = temp;
+			}
+		}
+	}
+
+	printf("Array after implementing bubble sort: \n");
+	for(x = 0; x < num; x++)
+	{
+		printf("%d \n", arr[x]);
+	}
+
+	return 0;
+
+}
